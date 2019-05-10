d220804c9d9e95736d607bf3e27f80f88654b9e9
Computer <computer@Computers-MacBook-Air.local>
2019-05-09 22:24:23 -0700

    tested pdb

--- median_of_arrays.py
+++ median_of_arrays.py
@@ -1,3 +1,5 @@
+import pdb;
+
 # find the median of two sorted arraysself.
 # eg
 # arr1 = [1,3,5]
@@ -9,19 +11,19 @@
 #2. How do you want to deal with the two median elements (this example is averaging them, but he may said just return the two median elements)
 
 #the O(n) way not acceptable in interview
-def find_median_of_arrays(arr1, arr2):
-    arr1.extend(arr2) # this merges arrays
-    arr1.sort()
-
-    n=len(arr1)
-
-    if n % 2 == 0: # if array is odd we need to take average  + middle -1
-        return (arr1[n/2] + arr1[n/2 -1]) / 2.0 #make one number float for python 2
-    else:
-        return arr1[n/2] # if array is even we need to take middle number
-
-print("answer for odd array " + str(find_median_of_arrays([1,2,3],[4,5,6,7])))
-print("answer for even array " + str(find_median_of_arrays([1,2,3],[4,5,6,])))
+# def find_median_of_arrays(arr1, arr2):
+#     arr1.extend(arr2) # this merges arrays
+#     arr1.sort()
+#
+#     n=len(arr1)
+#
+#     if n % 2 == 0: # if array is odd we need to take average  + middle -1
+#         return (arr1[n/2] + arr1[n/2 -1]) / 2.0 #make one number float for python 2
+#     else:
+#         return arr1[n/2] # if array is even we need to take middle number
+#
+# print("answer for odd array " + str(find_median_of_arrays([1,2,3],[4,5,6,7])))
+# print("answer for even array " + str(find_median_of_arrays([1,2,3],[4,5,6,])))
 
 
 #the O(log n) way - this is the respose you need to provide in interviews
@@ -31,16 +33,17 @@ print("answer for even array " + str(find_median_of_arrays([1,2,3],[4,5,6,])))
 # 2. repeatedly eliminate half (or any fraciton) of the elements in the rage
 
 
-# We consider array1[i] to be the first element in array1
-# We consider array2[x] to be the first element in array2
+# We consider array1[i] to be the first element in array1, index = 0
+# We consider array2[x] to be the first element in array2, index = 0
 def getKth(array1, i, array2, x, k):
-    if i == len(array1):
+    if i == len(array1): #checks for empty array1
         return array2[x + k]
-    elif x == len(array2):
+    elif x == len(array2): #checks for empty array2
         return array1[i + k]
-    elif k == 0:
+    elif k == 0: # if combined total_nums = 1, then total_nums/2 = 0
         return min(array1[i], array2[x])
 
+    # code below will execute if all the conditions above are false
     mid1 = min(len(array1) - i, (k + 1)/2)
     mid2 = min(len(array2) - x, (k + 1)/2)
     a = array1[i + mid1 - 1]
@@ -53,15 +56,16 @@ def getKth(array1, i, array2, x, k):
 #This function assumes that we have at least 1 number in the array
 def findMedianSortedArrays(nums1, nums2):
     total_nums = len(nums1) + len(nums2)
-    midpoint = total_nums/2 + 1
 
     if total_nums % 2 == 0:
         first = getKth(nums1, 0, nums2, 0, total_nums/2 -1)
         second = getKth(nums1, 0, nums2, 0, total_nums/2)
         return (first + second) / 2.0
+
     else:
-        return getKth(nums1, 0, nums2, 0, total_nums//2)
+        return getKth(nums1, 0, nums2, 0, total_nums/2)
 
 # print getKth([1,2,3],0,[4,5,6],0,2)
 # print getKth([1,2,3],0,[4,5,6],0,3)
 print findMedianSortedArrays([1,2,3], [4,5,6])
+pbd.set_trace()
